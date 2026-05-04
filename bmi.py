4f7e15f829fa05b6c2e9737fcc1423cce5adba98
Wanker 2659 <mrpandiman81@gmail.com>
2026-05-04 13:44:51 +0800

    Updated bmi code

--- bmi.py
+++ bmi.py
@@ -7,11 +7,14 @@ def calculate_bmi(height, weight):
     
     if bmi < 18.5:
         print("Underweight")
+        print(-1)
     
     elif bmi > 25:
         print("Overweight")
+        print(1)
     
     else:
         print("Normal weight")
+        print(0)
 
-calculate_bmi(weight=57, height=1.73)
+calculate_bmi(weight=85, height=1.73)
